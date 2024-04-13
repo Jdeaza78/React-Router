97703e0df8a166acab404f29df946d8c0f5f9070
Jdeaza78 <159087749+Jdeaza78@users.noreply.github.com>
2024-04-12 22:04:54 -0400

    updating data and URL params in loader

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,9 @@ import {
 import './index.css'
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-import Root, { loader as rootLoader } from "./routes/root";
+import Contact, { loader as contactLoader, } from "./routes/contact";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
@@ -27,6 +29,12 @@ const router = createBrowserRouter([
   {
     path: "contacts/:contactId",
     element: <Contact />,
+    loader: contactLoader,
+  },
+  {
+    path: "contacts/:contactId/edit",
+    element: <EditContact />,
+    loader: contactLoader,
   },
 ]);
 
